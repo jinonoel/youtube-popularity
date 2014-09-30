a360fe2c5ed37ac2091d59aa3b029e9d42ddc6a9
Joseph Christian Noel <jino@vpn-0a0f0065.ipt.nicta.com.au>
2014-09-30 22:12:23 +1000

    Fix youtube crawling bugs

--- crawling/sync_youtube_info.py
+++ crawling/sync_youtube_info.py
@@ -2,6 +2,7 @@ import sys
 import argparse
 import datetime
 import calendar
+import pymongo
 
 sys.path.append('/Users/jnoel/YTCrawl')
 import crawler
@@ -10,11 +11,13 @@ import crawler
 parser = argparse.ArgumentParser()
 parser.add_argument('start_date')
 parser.add_argument('end_date')
+parser.add_argument('delta')
 args = parser.parse_args()
 
 start_date = datetime.datetime.strptime(args.start_date, '%Y-%m-%d')
 end_date = datetime.datetime.strptime(args.end_date, '%Y-%m-%d')
 current_date = start_date
+delta = datetime.timedelta(int(args.delta))
 
 conn = pymongo.MongoClient('localhost')
 db = conn['nicta']
@@ -23,10 +26,11 @@ coll = db['videos']
 crawler = crawler.Crawler()
 
 while current_date < end_date:
-    print date
-
+    print current_date
+    mr_name = 'features_' + str(current_date).split()[0]
+ 
     i = 0
-    for result in coll.find({}, ['_id'], timeout=false):
+    for result in mr_name.find({}, ['_id'], timeout=False):
         vid_id = result['_id']
 
         i += 1
@@ -38,5 +42,7 @@ while current_date < end_date:
             coll.insert(data)
         except:
             print "Unexpected error:", sys.exc_info()[0]
+
+    current_date += delta
 
 db.close()
