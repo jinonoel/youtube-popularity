2b2dba2dc4aa5f6a4d380ae66e040cfa6d21a66c
Jino Noel <jinonoel@gmail.com>
2014-11-07 23:43:08 +0800

    New wait of generating features and labels

---
+++ feature_generation/generate_features_new.py
@@ -0,0 +1,40 @@
+import sys
+import argparse
+import datetime
+import pymongo
+
+parser = argparse.ArgumentParser()
+parser.add_argument('date')
+parser.add_argument('output_file')
+args = parser.parse_args()
+
+conn = pymongo.MongoClient('localhost')
+db = conn['nicta']
+
+video_features = {}
+
+output = open(args.output_file, 'w')
+
+coll_name = 'features_' + date + '_10'
+feature_coll = db[coll_name]
+
+i = 0
+written = 0
+
+for result in feature_coll.find():
+    i += 1
+    if i % 100000 == 0:
+        print i, written
+
+    vid_id = result['_id']
+    has_mention = result['value']['has_mention']
+    has_hashtag = result['value']['has_hashtag']
+    is_rt = result['value']['is_rt']
+    is_nbc = result['value']['is_nbc']
+    tweet_count = result['value']['tweet_count']
+    
+    written += 1
+    output.write(vid_id + ',' + str(has_mention) + ',' + str(has_hashtag) + ',' + str(is_rt) + ',' + str(is_nbc) + ',' + str(tweet_count) + '\n')
+
+
+output.close()
---
+++ feature_generation/generate_labels_new.py
@@ -0,0 +1,90 @@
+import sys
+import argparse
+import datetime
+import pymongo
+
+parser = argparse.ArgumentParser()
+parser.add_argument('start_date')
+parser.add_argument('A')
+parser.add_argument('B')
+parser.add_argument('output_file')
+args = parser.parse_args()
+
+start_date = args.start_date
+A_days = int(args.A)
+B_days = int(args.B)
+
+day_0 = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+day_A = day_0 + datetime.timedelta(A_days)
+day_B = day_0 + datetime.timedelta(B_days)
+
+A_date = str(day_A).split()[0]
+B_date = str(day_B).split()[0]
+
+conn = pymongo.MongoClient('localhost')
+db = conn['nicta']
+coll = db['videos']
+
+
+video_views = {}
+
+i = 0
+less = 0
+for result in coll.find({'uploadDate' : {
+        '$gte' : start_date,
+        '$lt' : A_date
+}}):
+
+    i += 1
+    if i % 100 == 0:
+        print i, len(video_views), len(daily_counts), less
+
+    vid_id = result['video_id'].split('#')[0]
+    if vid_id in video_views:
+        continue
+    
+    upload_date = datetime.datetime(result['uploadDate'], '%Y-%m-%d')
+    days_required = (day_B - upload_date).days + 1
+    baseline_days = (days_A - upload_date).days + 1
+
+    daily_counts = result['dailyViewCount']
+
+    if len(daily_counts) < days_required:
+        less += 1
+        continue
+
+    total_views = 0
+    baseline_views = 0
+
+    for j in range(baseline_days):
+        baseline_views += daily_counts[j]
+
+    for j in range(days_required):
+        total_views += daily_counts[j]
+
+    video_views[vid_id] = {
+        'views' : total_views,
+        'baselineViews' : baseline_views,
+        'uploadDate' : upload_date
+    }
+
+print 'Videos:', len(video_views)
+
+sorted_vids = sorted(video_views, key=lambda vid: video_views[vid]['views'], reverse=True)
+positive_count = int(len(video_views) * 0.05)
+j = 0
+
+output = open(args.output_file, 'w')
+baseline_file = open(args.baseline_file, 'w')
+
+for vid in sorted_vids:
+    if (j < positive_count):
+        output.write(vid + ",1," + str(video_views[vid]['views']) + "," + video_views[vid]['uploadDate'] + "," + A_date + "\n")
+        j += 1
+    else:
+        output.write(vid + ",0," + str(video_views[vid]['views']) + "," + video_views[vid]['uploadDate'] + "," + A_date + "\n")
+
+    baseline_file.write(vid + "," + str(video_views[vid]['baselineViews']) + "\n")
+
+output.close()
+baseline_file.close()
