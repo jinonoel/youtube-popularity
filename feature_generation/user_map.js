f3869d0fb4c5708ea993f9574dccbaa8a26ba7bd
Jino Noel <jinonoel@gmail.com>
2014-12-06 15:37:39 +0800

    Add missing active user features

--- feature_generation/user_map.js
+++ feature_generation/user_map.js
@@ -11,10 +11,18 @@ function() {
 
     var key = this.user.screen_name;
 
+    var userMentions = [];
+
     if (typeof entities != 'undefined') {
 	if (typeof entities.user_mentions != 'undefined' && entities.user_mentions.length > 0) {
 	    hasMention = 1;
 	    if (text.charAt(0) == '@') isNBC = 1;
+
+            for (var i = 0; i < entities.user_mentions.length; i++) {
+		if (typeof entities.user_mentions[i].screen_name != 'undefined' && typeof entities.user_mentions[i].screen_name.length > 0) {
+		    userMentions.push(entities.user_mentions[i].screen_name);
+		}
+            }
 	}
 
 	if (typeof entities.hashtags != 'undefined' && entities.hashtags.length > 0) {
@@ -26,12 +34,32 @@ function() {
 	}
     }
 
+    var hasHashtagInteraction = 0;
+    var isRTInteraction = 0;
+
+    if (hasMention > 0 && hasHashtag > 0) {
+	hasHashtagInteraction = 1;
+    }
+
+    if (hasMention > 0 && isRT > 0) {
+	isRTInteraction = 1;
+    }
+
     userData = {
 	has_mention : hasMention,
 	has_hashtag : hasHashtag,
+	has_hashtag_interaction : hasHashtagInteraction,
 	is_rt : isRT,
+	is_rt_interaction : isRTInteraction,
 	is_nbc : isNBC,
         tweet_count : 1,
+	user_mentions : userMentions,
+	user_interactions : {
+	    mention : [],
+	    hashtag : [],
+	    rt : [],
+	    nbc : []
+	}
     };
     
     emit(key, userData);
--- feature_generation/user_reduce.js
+++ feature_generation/user_reduce.js
@@ -2,17 +2,48 @@ function(key, values) {
     var userData = {
 	has_mention : 0,
 	has_hashtag : 0,
+	has_hashtag_interaction : 0,
 	is_rt : 0,
+	is_rt_interaction : 0,
 	is_nbc : 0,
-	tweet_count : 0
+	tweet_count : 0,
+	user_mentions : [],
+	user_interactions : {
+	    mention : [],
+	    hashtag : [],
+	    rt : [],
+	    nbc : []
+	}
     }
    
     for (var i = 0; i < values.length; i++) {
 	userData.has_mention += values[i].has_mention;
 	userData.has_hashtag += values[i].has_hashtag;
+	userData.has_hashtag_interaction += values[i].has_hashtag_interaction;
 	userData.is_rt += values[i].is_rt;
+	userData.is_rt_interaction += values[i].is_rt_interaction;
 	userData.is_nbc += values[i].is_nbc;
 	userData.tweet_count += values[i].tweet_count;
+
+	for (var j = 0; j < values[i].user_mentions.length; j++) {
+	    user = values[i].user_mentions[j];
+
+	    if (values[i].has_mention > 0) {
+		userData.user_interactions.mention.push(user);
+	    }
+
+	    if (values[i].has_hashtag > 0) {
+		userData.user_interactions.hashtag.push(user);
+	    }
+
+	    if (values[i].is_rt > 0) {
+		userData.user_interactions.rt.push(user);
+	    }
+
+	    if (values[i].is_nbc > 0) {
+		userData.user_interactions.nbc.push(user);
+	    }
+	}
     }
     
     return userData;
