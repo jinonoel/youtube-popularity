04ab60c870c543a2a5927888502cf2a6f7cb92cf
Jino Noel <jinonoel@gmail.com>
2014-12-09 23:16:23 +0800

    Proper way of calculating user-user interactions

--- feature_generation/user_map.js
+++ feature_generation/user_map.js
@@ -12,54 +12,56 @@ function() {
     var key = this.user.screen_name;
 
     var userMentions = [];
+    var userHashtags = [];
+    var userRTs = [];
+    var userNBCs = [];
+
 
     if (typeof entities != 'undefined') {
 	if (typeof entities.user_mentions != 'undefined' && entities.user_mentions.length > 0) {
 	    hasMention = 1;
-	    if (text.charAt(0) == '@') isNBC = 1;
+	    if (text.charAt(0) == '@') {
+		isNBC = 1;
+	    }
 
             for (var i = 0; i < entities.user_mentions.length; i++) {
 		if (typeof entities.user_mentions[i].screen_name != 'undefined' && entities.user_mentions[i].screen_name.length > 0) {
 		    userMentions.push(entities.user_mentions[i].screen_name);
+
+		    if (entities.user_mentions[i].indices[0] == 0) {
+			userNBCs.push(entities.user_mentions[i].screen_name);
+		    }
 		}
             }
 	}
 
 	if (typeof entities.hashtags != 'undefined' && entities.hashtags.length > 0) {
 	    hasHashtag = 1;
+
+	    for (var i = 0; i < entities.hashtags.length; i++) {
+		if (typeof entities.hashtags[i].text != 'undefined' && entities.hashtags[i].text.length > 0) { 
+		    userHashtags.push(entities.hashtags[i].text);
+		}
+            }
 	}
 
 	if (typeof this.retweeted_status != 'undefined' && typeof this.retweeted_status.id != 'undefined') {
 	    isRT = 1;
+
+	    userRTs.push(this.retweeted_status.user.screen_name);
 	}
-    }
-
-    var hasHashtagInteraction = 0;
-    var isRTInteraction = 0;
-
-    if (hasMention > 0 && hasHashtag > 0) {
-	hasHashtagInteraction = 1;
-    }
-
-    if (hasMention > 0 && isRT > 0) {
-	isRTInteraction = 1;
     }
 
     userData = {
 	has_mention : hasMention,
 	has_hashtag : hasHashtag,
-	has_hashtag_interaction : hasHashtagInteraction,
 	is_rt : isRT,
-	is_rt_interaction : isRTInteraction,
 	is_nbc : isNBC,
         tweet_count : 1,
-	user_mentions : userMentions,
-	user_interactions : {
-	    mention : [],
-	    hashtag : [],
-	    rt : [],
-	    nbc : []
-	}
+	mentions : userMentions,
+	hashtags : userHashtags,
+	rts :userRTs,
+	nbcs : userNBCs
     };
     
     emit(key, userData);
--- feature_generation/user_reduce.js
+++ feature_generation/user_reduce.js
@@ -2,47 +2,84 @@ function(key, values) {
     var userData = {
 	has_mention : 0,
 	has_hashtag : 0,
-	has_hashtag_interaction : 0,
 	is_rt : 0,
-	is_rt_interaction : 0,
 	is_nbc : 0,
 	tweet_count : 0,
-	user_mentions : [],
-	user_interactions : {
-	    mention : [],
-	    hashtag : [],
-	    rt : [],
-	    nbc : []
-	}
+	mentions : [],
+	hashtags : [],
+	rts : [],
+	nbcs : []
     }
    
     for (var i = 0; i < values.length; i++) {
 	userData.has_mention += values[i].has_mention;
 	userData.has_hashtag += values[i].has_hashtag;
-	userData.has_hashtag_interaction += values[i].has_hashtag_interaction;
 	userData.is_rt += values[i].is_rt;
-	userData.is_rt_interaction += values[i].is_rt_interaction;
 	userData.is_nbc += values[i].is_nbc;
 	userData.tweet_count += values[i].tweet_count;
 
-	for (var j = 0; j < values[i].user_mentions.length; j++) {
-	    user = values[i].user_mentions[j];
-	    userData.user_mentions.push(user);
+	for (var j = 0; j < values[i].mentions.length; j++) {
+	    var exists = false;
+	    user = values[i].mentions[j];
 
-	    if (values[i].has_mention > 0) {
-		userData.user_interactions.mention.push(user);
+	    for (var k = 0; k < userData.mentions.length; k++) {
+		if (userData.mentions[k] == user) {
+		    exists = true;
+		    break;
+		}
 	    }
 
-	    if (values[i].has_hashtag > 0) {
-		userData.user_interactions.hashtag.push(user);
+	    if (!exists) {
+		userData.mentions.push(user);
+	    }
+	}
+	
+
+	for (var j = 0; j < values[i].hashtags.length; j++) {
+	    var exists = false;
+	    user = values[i].hashtags[j];
+
+	    for (var k = 0; k < userData.hashtags.length; k++) {
+		if (userData.hashtags[k] == user) {
+		    exists = true;
+		    break;
+		}
 	    }
 
-	    if (values[i].is_rt > 0) {
-		userData.user_interactions.rt.push(user);
+	    if (!exists) {
+		userData.hashtags.push(user);
+	    }
+	}
+
+	for (var j = 0; j < values[i].rts.length; j++) {
+	    var exists = false;
+	    user = values[i].rts[j];
+
+	    for (var k = 0; k < userData.rts.length; k++) {
+		if (userData.rts[k] == user) {
+		    exists = true;
+		    break;
+		}
 	    }
 
-	    if (values[i].is_nbc > 0) {
-		userData.user_interactions.nbc.push(user);
+	    if (!exists) {
+		userData.rts.push(user);
+	    }
+	}
+
+	for (var j = 0; j < values[i].nbcs.length; j++) {
+	    var exists = false;
+	    user = values[i].nbcs[j];
+
+	    for (var k = 0; k < userData.nbcs.length; k++) {
+		if (userData.nbcs[k] == user) {
+		    exists = true;
+		    break;
+		}
+	    }
+
+	    if (!exists) {
+		userData.nbcs.push(user);
 	    }
 	}
     }
